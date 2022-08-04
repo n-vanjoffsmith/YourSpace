010381d64b2052b86311c03ec8d8bc9b47308fe5
Dakshata Chidambaram <chidambaramdakshata@gmail.com>
2022-08-04 07:49:37 -0700

    added website link

--- YourSpace/ViewController.swift
+++ YourSpace/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+
+   
+    @IBOutlet weak var linkButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        linkButton.addTarget(self,action:#selector(openLink),for: .touchUpInside)
+    
     }
 
 
+    
+    @objc func openLink() {
+        // If let only runs the code inside if it can succesfuly unwrap the optional URL object.
+        
+        if let urlToOpen = URL(string: "https://docs.google.com/document/d/14Yfux5rvEwWyHPjlm7CODSblTQcYWkzF-V0fBoaHYMQ/edit?usp=sharing") {
+            // Open the URL in the preferred browser
+            UIApplication.shared.open(urlToOpen,options: [:]) { (done) in
+                
+                
+                print("Link was opened succesfully")
+
+            }
+            
+            
+        }
+    }
 }
+
 
